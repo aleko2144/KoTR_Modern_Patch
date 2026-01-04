80f23b0e09f57fa6263bcf38ed858658434debf5
aleko2144 <42273133+aleko2144@users.noreply.github.com>
2026-01-04 15:24:41 +0300

    Uploaded the actual version

--- KoTR_CollisionMod/AsiPlugin/dllmain.cpp
+++ KoTR_CollisionMod/AsiPlugin/dllmain.cpp
@@ -1,64 +1,12 @@
 #include <windows.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
-#include <cmath>
-
-#include <fstream>
-
 #include "dllmain.h"
-
 #include "CPatch.h"
 
 using namespace std;
 
-float GetPrivateProfileFloat(string selection, string varname, string default_val, string filename){
-	float to_return;
-	char* returnedString = new char[256];
-	GetPrivateProfileStringA(selection.c_str(), varname.c_str(), default_val.c_str(), returnedString, 256, filename.c_str());
-	to_return = atof(returnedString);
-	return to_return;
-}
-
-
-float friction_coeff = 1.4;
-float collision_coeff = 0.5;
-
-float* friction_coeff_ptr = (float*)0x696BFC;
-float* collision_coeff_ptr = (float*)0x696C00; 
-
-void CALLBACK OnTimer(HWND hwnd, UINT msg, UINT_PTR idTimer, DWORD dwTime)
-{
-	if (*friction_coeff_ptr != friction_coeff) {
-		*friction_coeff_ptr = friction_coeff;
-	}
-
-	if (*collision_coeff_ptr != collision_coeff) {
-		*collision_coeff_ptr = collision_coeff;
-	}
-}
-
-int *__cdecl sub_52EF20(int *a1, int a2, int a3, int a4, int a5, int a6, int a7){
-	typedef int *(*sub_52EF20)(int *a1, int a2, int a3, int a4, int a5, int a6, int a7);
-	return sub_52EF20(0x52EF20)(a1, a2, a3, a4, a5, a6, a7);
-}
-
-int *__cdecl sub_52EF20_hook(int *a1, int EDX, int a2, int a3, int a4, int a5, int a6, int a7){
-	return 0;
-}
-
-signed int __cdecl sub_5C88F0(DWORD *a1, int a2){
-	typedef signed int(*sub_5C88F0)(DWORD *a1, int a2);
-	return sub_5C88F0(0x5C88F0)(a1, a2);
-}
-
-signed int __cdecl sub_5C88F0_hook(DWORD *a1, int *EDX, int a2){
-	return 1;
-}
-
 BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
 {
-
 	switch(fdwReason)
 	{
 		case DLL_PROCESS_ATTACH:
@@ -66,14 +14,13 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
 			if (!GetPrivateProfileIntA("MOD", "Enabled", 0, ".\\KoTR_CollisionMod.ini"))
 				return TRUE;
 
-			SetTimer(0, 0, 50, OnTimer);
+			//replace default collision_coeff in truck.ini read function
+			CPatch::SetFloat(0x4DFAE2, 0.75);
 
-			friction_coeff = GetPrivateProfileFloat("CAR", "friction_coeff", "1.4", ".\\KoTR_CollisionMod.ini");
-			collision_coeff = GetPrivateProfileFloat("CAR", "collision_coeff", "0.5", ".\\KoTR_CollisionMod.ini");
+			//replace pointer of dbl 0.2 to 3.141 inside slide collision process function
+			CPatch::SetInt(0x57FF08, 6600744);
 
-			CPatch::RedirectJump(0x5C88F0, sub_5C88F0_hook);
-
-			cout << "CollisionPatch v0.5 (03.11.2025) started." << endl;
+			cout << "CollisionPatch v0.51 (04.01.2026) started." << endl;
 
 			break;
 		}
@@ -91,6 +38,5 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
 		}
 	}
 
-	/* Return TRUE on success, FALSE on failure */
 	return TRUE;
 }
