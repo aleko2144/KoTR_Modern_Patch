962a83d81a4ff89448cd827e337867266d150b91
aleko2144 <42273133+aleko2144@users.noreply.github.com>
2026-01-20 23:21:38 +0300

    better rain solution

--- KoTR_GameFixes/AsiPlugin/sources/include/HorizontalRainFix.cpp
+++ KoTR_GameFixes/AsiPlugin/sources/include/HorizontalRainFix.cpp
@@ -1,25 +1,25 @@
 #include "HorizontalRainFix.h"
 #include "utils\CPatch.h"
 
+double newRainHSpeed = 50.0;
 void __fastcall HorizontalRainFix::OnProcessRainDrop(int *_this, int* EDX){
 	int* TheGame = (int*)((char*)_this + 0x20);
-	double* dt = (double*)((char*)*TheGame + 0xB90);
-	double prevDt = *dt;
+	double dt = *(double*)((char*)*TheGame + 0xB90); //delta time
 
-	//std::cout << prevDt << std::endl;
+	//default game value is dt * 50.0 = 1.5 at 30 FPS
+	newRainHSpeed = 1.5 / dt;
 
-	//if the game is running too fast, let the rain drop object instance think that the game is running at 25 FPS
-	if (*dt < 0.04)
-		*dt = 0.04;
+	//limit value (original in-game value is 50.0)
+	if (newRainHSpeed < 50.0)
+		newRainHSpeed = 50.0;
 
 	//call original function
 	((int(__thiscall *)(int*))0x5A5ED0)(_this);
-
-	//restore delta time value
-	*dt = prevDt;
 }
 
 void HorizontalRainFix::injectHooks(){
-	//replace function call pointers
+	//replace function call pointer
 	CPatch::SetPointer(0x651534, &OnProcessRainDrop);
+	//replace rain drop moving down speed value
+	CPatch::SetPointer(0x5A5EE4, &newRainHSpeed);
 }
