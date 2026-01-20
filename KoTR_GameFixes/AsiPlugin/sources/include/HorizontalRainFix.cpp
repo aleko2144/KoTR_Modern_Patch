266f8782e0aff35d86aa8aa996fcf2049d3bfc30
Andrey <42273133+aleko2144@users.noreply.github.com>
2026-01-20 23:29:59 +0300

    Update HorizontalRainFix.cpp

--- KoTR_GameFixes/AsiPlugin/sources/include/HorizontalRainFix.cpp
+++ KoTR_GameFixes/AsiPlugin/sources/include/HorizontalRainFix.cpp
@@ -1,17 +1,17 @@
 #include "HorizontalRainFix.h"
 #include "utils\CPatch.h"
 
-double newRainHSpeed = 50.0;
+double rainSpeed = 50.0;
 void __fastcall HorizontalRainFix::OnProcessRainDrop(int *_this, int* EDX){
 	int* TheGame = (int*)((char*)_this + 0x20);
 	double dt = *(double*)((char*)*TheGame + 0xB90); //delta time
 
 	//default game value is dt * 50.0 = 1.5 at 30 FPS
-	newRainHSpeed = 1.5 / dt;
+	rainSpeed = 1.5 / dt;
 
 	//limit value (original in-game value is 50.0)
-	if (newRainHSpeed < 50.0)
-		newRainHSpeed = 50.0;
+	if (rainSpeed < 50.0)
+		rainSpeed = 50.0;
 
 	//call original function
 	((int(__thiscall *)(int*))0x5A5ED0)(_this);
@@ -21,5 +21,5 @@ void HorizontalRainFix::injectHooks(){
 	//replace function call pointer
 	CPatch::SetPointer(0x651534, &OnProcessRainDrop);
 	//replace rain drop moving down speed value
-	CPatch::SetPointer(0x5A5EE4, &newRainHSpeed);
+	CPatch::SetPointer(0x5A5EE4, &rainSpeed);
 }
