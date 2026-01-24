20558b1fb1f4b8c508ebba27f7b8624727398ff9
aleko2144 <42273133+aleko2144@users.noreply.github.com>
2026-01-24 13:29:59 +0300

    Update AICarFix.cpp

--- KoTR_GameFixes/AsiPlugin/sources/Hooks/AICarFix.cpp
+++ KoTR_GameFixes/AsiPlugin/sources/Hooks/AICarFix.cpp
@@ -13,20 +13,27 @@ LocalRoadParameters roadParams;
 
 double dbl_dist = 150.0, dbl_incr = 30.0;
 double new_dist, new_incr;
+float  max_wdth;
 
-bool __cdecl sub_4CFD50h(int *playerID){
+bool __cdecl OnCall4CFD50(int *playerID){
 	bool result = sub_4CFD50(playerID);
 
 	if (result){
+		//1. получаем PositionId текущего водителя
 		//1. getting player position ID
 		getPlayerPositionId(&posId, playerID);
+
+		//2. получаем параметры дороги по PositionId
 		//2. getting road parameters by position ID
 		getRoadParameters(posId, &roadParams);
+
+		//3. если дорога узкая (2-х полоска), то меняем параметры
 		//3. if road is narrow (2 lanes or less) then change values
-		if (roadParams.roadWidth < 10.0){
+		if (roadParams.roadWidth < max_wdth){
 			dbl_dist = new_dist;
 			dbl_incr = new_incr;
 		} else {
+			//если нет, возвращаем параметры по умолчанию
 			//default values
 			dbl_dist = 150.0;
 			dbl_incr = 30.0;
@@ -40,14 +47,15 @@ bool __cdecl sub_4CFD50h(int *playerID){
 
 void AICarFix::injectHooks(){
 	new_dist = GetPrivateProfileDouble("AI_CAR", "loop_dist", "150.0", ".\\KoTR_ModernPatch.ini");
-	new_incr = GetPrivateProfileDouble("AI_CAR", "loop_incr", "30.0", ".\\KoTR_ModernPatch.ini");
+	new_incr = GetPrivateProfileDouble("AI_CAR", "loop_incr", "30.0",  ".\\KoTR_ModernPatch.ini");
+	max_wdth = GetPrivateProfileFloat ("AI_CAR", "max_width", "10.0",  ".\\KoTR_ModernPatch.ini");
 
 	CPatch::SetPointer(0x4D528E, &dbl_dist);
 	CPatch::SetPointer(0x4D4EAF, &dbl_dist);
 	CPatch::SetPointer(0x4D5284, &dbl_incr);
 	
 	//hook function before do/while cycle
-	CPatch::RedirectCall(0x4D4FC2, &sub_4CFD50h);
+	CPatch::RedirectCall(0x4D4FC2, &OnCall4CFD50);
 
 	//std::cout << "roadParamsAddr=" << std::hex << &roadParams << std::endl;
 }
