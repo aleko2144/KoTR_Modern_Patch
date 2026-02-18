cb4982d7aa559d06894e135665a309c67a8db124
aleko2144 <42273133+aleko2144@users.noreply.github.com>
2026-02-18 23:47:52 +0300

    Update dllmain.cpp

--- KoTR_ModernPatch/AsiPlugin/sources/dllmain.cpp
+++ KoTR_ModernPatch/AsiPlugin/sources/dllmain.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+#include "Utils\ExeUtils.h"
+
 //patches
 #include "Hooks\MirrorsFix.h"
 #include "Hooks\CollisionsFix.h"
@@ -30,6 +32,19 @@ void printInfo(const char* text){
 	if (printDebugInfo)
 		cout << text << endl;
 }
+void printGameVersion(int version) {
+	if (!printDebugInfo)
+		return;
+
+	cout << "Detected game version: ";
+
+	if (version){
+		//просто чтобы текст был красивый / just to make the text look nice :)
+		cout << (version / 10) % 10 << "." << version % 10 << endl;
+	} else {
+		cout << "unknown (failed to detect)" << endl;
+	}
+}
 void displayConsole(){
 	AllocConsole();
 	freopen("conin$","r", stdin);
@@ -47,68 +62,80 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 		case DLL_PROCESS_ATTACH:
 		{
 			LPCSTR configName = ".\\KoTR_ModernPatch.ini";
+			int gameVersion = GetGameVersion();
 
 			printDebugInfo = GetPrivateProfileIntA("common", "DisplayConsole", 0, configName);
-			if (printDebugInfo){
+			if (printDebugInfo)
 				displayConsole();
-				printInfo("Modern Patch v1.03");
-			}
 
-			if (GetPrivateProfileIntA("patches", "MirrorsFix", 0, configName)){
-				MirrorsFix::injectHooks();
-				printInfo("[PATCH]: injected mirror system patches");
-			}
+			printInfo("Modern Patch v1.03");
+			printGameVersion(gameVersion);
 
-			if (GetPrivateProfileIntA("patches", "DustParticlesFix", 0, configName)){
-				DustParticlesFix::injectHooks();
-				printInfo("[PATCH]: injected dust particle patches");
-			}
-
-			if (GetPrivateProfileIntA("patches", "TechSystemFix", 0, configName)){
-				TechSystemFix::injectHooks();
-				printInfo("[PATCH]: injected vehicle upgrade system patches");
-			}
-
-			if (GetPrivateProfileIntA("patches", "CollisionsFix", 0, configName)){
-				CollisionsFix::injectHooks();
-				printInfo("[PATCH]: applied collision corrections");
-			}
-
-			if (GetPrivateProfileIntA("patches", "CabinCamFix", 0, configName)){
-				if (CabinCamFix::injectHooks()) {
-					printInfo("[PATCH]: applied interior camera tweaks");
+			if (GetPrivateProfileIntA("patches", "QueueSortingFix", 0, configName)) {
+				if (QueueSortingFix::injectHooks()) {
+					printInfo("[PATCH]: applied queue sorting fixes");
 				}
 			}
 
-			if (GetPrivateProfileIntA("AI_CAR", "HookEnabled", 0, configName)){
-				AICarFix::injectHooks();
-				printInfo("[PATCH]: injected AI patches");
+			//needs to be fixed only in 8.x
+			if (GetPrivateProfileIntA("patches", "CabinCamFix", 0, configName) && gameVersion >= 80){
+				if (CabinCamFix::injectHooks()) {
+					printInfo("[PATCH]: fixed cabins height");
+				}
 			}
+
+			if (GetPrivateProfileIntA("patches", "MirrorsFix", 0, configName)){
+				if (MirrorsFix::injectHooks(gameVersion)) {
+					printInfo("[PATCH]: injected mirror system patches");
+				}
+			}
+
+			//old game versions doesn't need collision coeffs correction
+			if (GetPrivateProfileIntA("patches", "CollisionsFix", 0, configName) && gameVersion > 71) {
+				if (CollisionsFix::injectHooks()) {
+					printInfo("[PATCH]: applied collision corrections");
+				}
+			}
+
+			if (GetPrivateProfileIntA("EXT_CAMERA", "HookEnabled", 0, configName)) {
+				if (ExtCameraFix::injectHooks()) {
+					printInfo("[PATCH]: applied external camera tweaks");
+				}
+			}
+
+			if (GetPrivateProfileIntA("patches", "TechSystemFix", 0, configName) && gameVersion >= 80) {
+				if (TechSystemFix::injectHooks())
+					printInfo("[PATCH]: injected vehicle upgrade system patches");
+			}
+
+			if (GetPrivateProfileIntA("patches", "DustParticlesFix", 0, configName) && gameVersion >= 70) {
+				if (DustParticlesFix::injectHooks(gameVersion))
+					printInfo("[PATCH]: injected dust particle patches");
+			}
+
+			//patches for 8.2 only
+			if (gameVersion == 82) {
+
+				if (GetPrivateProfileIntA("AI_CAR", "HookEnabled", 0, configName)) {
+					AICarFix::injectHooks();
+					printInfo("[PATCH]: injected AI patches");
+				}
+
+				if (GetPrivateProfileIntA("STRAILER_PHY", "HookEnabled", 0, configName)) {
+					STrailersPhysFix::injectHooks();
+					printInfo("[PATCH]: applied roadtrain physics behaviour patches");
+				}
+			}
+
+			//445AF4 infinity loop if trailer detached on mafia attack???//
+
+			/////
 
 			//if (GetPrivateProfileIntA("patches", "FinesCorrection", 0, configName)){
 			//	FinesCorrection::injectHooks();
 			//	printInfo("[PATCH]: applied fines corrections");
 			//}
 
-			if (GetPrivateProfileIntA("STRAILER_PHY", "HookEnabled", 0, configName)){
-				STrailersPhysFix::injectHooks();
-				printInfo("[PATCH]: applied roadtrain physics behaviour patches");
-			}
-
-			//445AF4 infinity loop if trailer detached on mafia attack???//
-
-			if (GetPrivateProfileIntA("EXT_CAMERA", "HookEnabled", 0, configName)) {
-				ExtCameraFix::injectHooks();
-				printInfo("[PATCH]: applied camera tweaks");
-			}
-
-			if (GetPrivateProfileIntA("patches", "QueueSortingFix", 0, configName)){
-				if (QueueSortingFix::injectHooks()) {
-					printInfo("[PATCH]: applied queue sorting fixes");
-				}
-			}
-
-			/////
 			//rain fix and affinity limit disable now exists in the D2GI
 			//
 			//if (GetPrivateProfileIntA("patches", "HorizontalRainFix", 0, configName)){
@@ -122,10 +149,10 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 			//}
 			/////
 
-			if (GetPrivateProfileIntA("mods", "HT2Effects", 0, configName)) {
-				HT2EffectsMod::injectHooks();
-				printInfo("[MOD]: loaded HTruck 2 effects");
-			}
+			//if (GetPrivateProfileIntA("mods", "HT2Effects", 0, configName)) {
+			//	HT2EffectsMod::injectHooks();
+			//	printInfo("[MOD]: loaded HTruck 2 effects");
+			//}
 
 			break;
 		}
